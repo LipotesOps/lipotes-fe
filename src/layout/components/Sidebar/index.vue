a65ce1c7ae0ad47f7a0ef5aed79505482cac3198
Corwin Zhang <yanbing2020@gmail.com>
2020-07-16 23:45:41 +0800

    chore

--- src/layout/components/Sidebar/index.vue
+++ src/layout/components/Sidebar/index.vue
@@ -1,12 +1,53 @@
 <template>
-  <div>
-    <p>i am sidebar!</p>
-    <router-view />
+  <div :class="{ 'has-logo': showLogo }">
+    <p>sidebar</p>
+    <logo v-if="showLogo" :collapse="isCollapse" />
+    <el-scrollbar wrap-class="scrollbar-wrapper">
+      <el-menu
+        :default-active="activeMenu"
+        :collapse="isCollapse"
+        :background-color="variables.menuBg"
+        :text-color="variables.menuText"
+        :unique-opened="false"
+        :active-text-color="variables.menuActiveText"
+        :collapse-transition="false"
+        mode="vertical"
+      >
+        <sidebar-item
+          v-for="route in permission_routes"
+          :key="route.path"
+          :item="route"
+          :base-path="route.path"
+        />
+      </el-menu>
+    </el-scrollbar>
   </div>
 </template>
 
 <script>
-export default {};
+import variables from "@/styles/variables.scss";
+export default {
+  computed: {
+    activeMenu() {
+      const route = this.$route;
+      const { meta, path } = route;
+      // if set path, the sidebar will highlight the path you set
+      if (meta.activeMenu) {
+        return meta.activeMenu;
+      }
+      return path;
+    },
+    showLogo() {
+      return true;
+    },
+    variables() {
+      return variables;
+    },
+    isCollapse() {
+      return true;
+    }
+  }
+};
 </script>
 
 <style></style>

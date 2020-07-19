f1c99ec0170037c495cde5ac4cdfbc0b9c67ca34
Corwin Zhang <yanbing2020@gmail.com>
2020-07-19 15:34:57 +0800

    remove mock

--- src/api/user.js
+++ src/api/user.js
@@ -1,28 +1,5 @@
 import request from '@/utils/request'
 
-// export function login(data) {
-//   return request({
-//     url: '/vue-admin-template/user/login',
-//     method: 'post',
-//     data
-//   })
-// }
-
-// export function getInfo(token) {
-//   return request({
-//     url: '/vue-admin-template/user/info',
-//     method: 'get',
-//     params: { token }
-//   })
-// }
-
-// export function logout() {
-//   return request({
-//     url: '/vue-admin-template/user/logout',
-//     method: 'post'
-//   })
-// }
-
 const baseURL = 'http://127.0.0.1:8000'
 const auth = { username: 'admin', password: '20200077' }
 

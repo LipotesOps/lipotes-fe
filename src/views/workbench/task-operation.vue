3db20a2d237003a6fa69af58838df9a37b050fb6
Corwin Zhang <yanbing2020@gmail.com>
2020-09-10 13:35:28 +0800

    refine style

--- src/views/workbench/task-operation.vue
+++ src/views/workbench/task-operation.vue
@@ -1,5 +1,8 @@
 <template>
   <div class="app-container">
+    <div class="app-content-title">
+      <h4 style="margin:0px">{{ taskDetail.flow_instance.flow_bpmn.flow.name }} - {{ taskDetail.name }}</h4>
+    </div>
     <div class="app-content-container">
       <el-form :inline="true">
         <el-form-item label="工单标题">
@@ -11,7 +14,31 @@
 </template>
 
 <script>
+import { fetchTask } from '@/api/itsc-flow'
+
 export default {
+  data() {
+    return {
+      Title: '',
+      task_uuid: '',
+      taskDetail: ''
+    }
+  },
+  created() {
+    this.task_uuid = this.$route.params.task_uuid
+    this.getTaskDetail()
+  },
+  methods: {
+    getTaskDetail() {
+      const query = { uuid: this.task_uuid }
+      fetchTask(query).then(resp => {
+        if (resp.status === 200) {
+          this.taskDetail = resp.data.results[0]
+          console.log(this.taskDetail)
+        }
+      })
+    }
+  }
 
 }
 </script>
